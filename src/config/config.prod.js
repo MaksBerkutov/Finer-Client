9cd5c5ff5cc4b5828edacf4d5617ebca15c2fc4c
MaksBerkutov <07012004maks@gmail.com>
2023-11-29 16:06:48 +0200

    new host test

--- src/config/config.prod.js
+++ src/config/config.prod.js
@@ -1,6 +1,6 @@
 const config = {
-	serverUrl: 'https://finer-server.vercel.app:3003',
-	serverSocketUrl: 'https://finer-server.vercel.app:3004',
+	serverUrl: 'https://finer-server-production.up.railway.app:3003',
+	serverSocketUrl: 'https://finer-server-production.up.railway.app:3004',
 	clientUrl: 'https://finer-discord.vercel.app',
 }
 
