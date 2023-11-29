4b50bceb241e97604650ca64cf9c0b28b8f16a1f
MaksBerkutov <07012004maks@gmail.com>
2023-11-29 13:43:24 +0200

    migrate to new host

--- src/config/config.prod.js
+++ src/config/config.prod.js
@@ -1,7 +1,7 @@
 const config = {
 	serverUrl: 'none',
 	serverSocketUrl: 'none',
-	clientUrl: 'https://finer-two.vercel.app',
+	clientUrl: 'https://finer-discord.vercel.app',
 }
 
 export default config
