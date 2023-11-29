b44a83054c6ae949ad58578293af5b527bb8fc5c
MaksBerkutov <07012004maks@gmail.com>
2023-11-29 17:33:12 +0200

    Upd cfg

--- src/config/config.prod.js
+++ src/config/config.prod.js
@@ -1,6 +1,6 @@
 const config = {
 	serverUrl: 'https://finer-server-production.up.railway.app',
-	serverSocketUrl: 'https://finer-server-production.up.railway.app:3004',
+	serverSocketUrl: 'https://finer-server-production.up.railway.app',
 	clientUrl: 'https://finer-discord.vercel.app',
 }
 
