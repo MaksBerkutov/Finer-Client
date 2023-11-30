8c7773fcd1adda4b837a9d8adc8c6fe7d8a833ae
MaksBerkutov <07012004maks@gmail.com>
2023-11-30 14:25:21 +0200

    Minimum, uses socket

--- src/pages/MusicPage/MusicPlayer/MusicPlayer.jsx
+++ src/pages/MusicPage/MusicPlayer/MusicPlayer.jsx
@@ -1,18 +1,14 @@
 import React from 'react'
 import classes from './MusicPlayer.module.css'
 
-const MusicPlayer = ({ socket, text, id, playedStatus }) => {
-
-	const changePlayedStatuc = () => {
-		socket.emit('changeMusicStatus', { id: id, newStatus: !playedStatus })
-	}
-	const goNextSongs = () => {
-		socket.emit('goNextSongs', id)
-	}
-	const goPrevSongs = () => {
-		socket.emit('goPrevSongs', id)
-	}
-
+const MusicPlayer = ({
+	text,
+	id,
+	playedStatus,
+	next,
+	prev,
+	changePlayeStatus,
+}) => {
 	return (
 		<div className={classes.player}>
 			{text === '' ? (
@@ -22,11 +18,11 @@ const MusicPlayer = ({ socket, text, id, playedStatus }) => {
 			)}
 
 			<div className={classes.controls}>
-				<button onClick={() => goPrevSongs()}>Previous</button>
-				<button onClick={() => changePlayedStatuc()}>
+				<button onClick={() => prev()}>Previous</button>
+				<button onClick={() => changePlayeStatus()}>
 					{playedStatus ? 'Pause' : 'Resume'}
 				</button>
-				<button onClick={() => goNextSongs()}>Next</button>
+				<button onClick={() => next()}>Next</button>
 			</div>
 		</div>
 	)
