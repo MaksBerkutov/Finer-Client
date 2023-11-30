04d22bf3d69f6da65e4e90cfe4ec2aaea4b35e59
MaksBerkutov <07012004maks@gmail.com>
2023-11-30 11:20:56 +0200

    Add Suport mobile device

--- src/components/TopMenu/TopMenu.jsx
+++ src/components/TopMenu/TopMenu.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 import classes from './TopMenu.module.css'
 import TopMenuItem from './TopMenuItem/TopMenuItem'
 
@@ -9,32 +11,35 @@ const TopMenu = ({
 	LastItemMenu,
 }) => {
 	return (
-		<nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
-			<div className='container '>
+		<Navbar expand='lg' variant='dark' bg='dark'>
+			<div className='container'>
 				{LogoItem !== undefined && <>{LogoItem}</>}
 
-				<div className='collapse navbar-collapse' id='navbarNav'>
-					<ul className='navbar-nav'>
-						{NavigationItemCenter?.map(Element => (
-							<TopMenuItem item={Element} />
+				<Navbar.Toggle aria-controls='navbarNav' />
+
+				<Navbar.Collapse id='navbarNav'>
+					<Nav className='mr-auto'>
+						{NavigationItemCenter?.map((Element, index) => (
+							<TopMenuItem key={index} item={Element} />
 						))}
-					</ul>
+					</Nav>
+
 					<div className={classes.rightMenu}>
 						<div className='d-flex flex-row justify-content-center align-items-center'>
-							<ul className='navbar-nav'>
-								{RightItemMenu?.map(Element => (
-									<TopMenuItem item={Element} />
+							<Nav>
+								{RightItemMenu?.map((Element, index) => (
+									<TopMenuItem key={index} item={Element} />
 								))}
-							</ul>
+							</Nav>
 
 							<div className={classes.avatarContainer}>
 								{LastItemMenu !== undefined && <>{LastItemMenu}</>}
 							</div>
 						</div>
 					</div>
-				</div>
+				</Navbar.Collapse>
 			</div>
-		</nav>
+		</Navbar>
 	)
 }
 
